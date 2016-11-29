18462c2078268d05e68f10a3fbd02349d120ab68
nicochaves <nico.chaves@yahoo.com>
2016-11-28 16:57:02 -0800

    Initial commit

---
+++ OCRTest/src/main/java/edu/sfsu/cs/orange/ocr/math/ExpressionParser.java
@@ -0,0 +1,16 @@
+package edu.sfsu.cs.orange.ocr.math;
+
+import com.fathzer.soft.javaluator.DoubleEvaluator;
+
+/**
+ * Created by nicochaves on 11/27/16.
+ */
+
+public class ExpressionParser {
+
+    public static Double parse(String exp) {
+        DoubleEvaluator evaluator = new DoubleEvaluator();
+        Double result = evaluator.evaluate(exp);
+        return result;
+    }
+}
